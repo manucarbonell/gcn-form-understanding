08af7d664b842cbd82860a23fdd4e85fa2508c46
manucarbonell <manucarbonell@github.com>
2020-04-23 12:37:51 +0200

    Graph visualization utils

--- utils.py
+++ utils.py
@@ -1,3 +1,10 @@
+import torch
+import pdb
+import dgl
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
 def adjacency_to_pairs_and_labels(am):
     pairs=[]
     labels=[]
@@ -8,4 +15,15 @@ def adjacency_to_pairs_and_labels(am):
 
     return pairs,labels
     
+def visualize_graph(g):
+    position = np.array(g.ndata['position'])
+    g = g.to_networkx()
+    nx.draw(g, pos=position, arrows=False)
+    plt.show()
+
+def edges_list_to_dgl_graph(edges):
+    g = dgl.DGLGraph()
+    g.add_nodes(int(torch.max(edges))+1)
+    g.add_edges(edges[:,0],edges[:,1])
+    return g
 
