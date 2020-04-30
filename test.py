7fb76cac0613b921742e2b1f5979d0278e9300fc
manucarbonell <manucarbonell@github.com>
2020-04-30 17:01:34 +0200

    Calculate ARI

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 import torch.utils.data as data
 import glob
 
+from utils import *
 from random import randrange
 from dgl.nn.pytorch import GATConv, GraphConv
 import matplotlib.pyplot as plt
@@ -15,6 +16,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 import os
 import sys
+import sklearn
 
 import numpy as np
 import xml.etree.ElementTree as ET
@@ -37,7 +39,7 @@ model = torch.load('model.pt')
 recalls = []
 precisions =[]
 print ("Validation on test set")
-
+aris = []
 for iter,(bg,blab) in enumerate(test_loader):
     print(float(iter)/len(test_loader),'\r\r')
     prediction = model(bg)
@@ -56,8 +58,44 @@ for iter,(bg,blab) in enumerate(test_loader):
 
     recalls.append(rec)
     precisions.append(prec)
+
+    # GT AND PRED COMPONENTS
+
+    pred_edges = torch.t(torch.stack([bg.edges()[0][prediction.bool()],bg.edges()[1][prediction.bool()]]))
+    predg = edges_list_to_dgl_graph(pred_edges)
+    predg.ndata['position']=bg.ndata['position']
+    
+    pred_components = nx.connected_components(predg.to_networkx().to_undirected())
+    
+    target_edges = torch.t(torch.stack([bg.edges()[0][target.bool()],bg.edges()[1][target.bool()]]))
+    yg = edges_list_to_dgl_graph(target_edges)
+    yg.ndata['position']=bg.ndata['position']
+    
+    gt_components = nx.connected_components(yg.to_networkx().to_undirected())
+    cluster_idx=0
+    pred_node_labels=np.zeros(bg.number_of_nodes())
+    all_nodes = []
+    for node_cluster in pred_components:
+        for node in node_cluster:
+            all_nodes.append(node)
+            pred_node_labels[node]=cluster_idx
+        cluster_idx+=1
+    cluster_idx=0
+    gt_node_labels=np.zeros(bg.number_of_nodes())
+
+    for node_cluster in gt_components:
+        for node in node_cluster:
+            gt_node_labels[node]=cluster_idx
+        cluster_idx+=1
+    
+    ari = sklearn.metrics.adjusted_rand_score(gt_node_labels,pred_node_labels)
+    aris.append(ari)
+
 epoch_prec = np.mean(precisions)
 epoch_rec = np.mean(recalls)
 print('Precision, recall:',epoch_prec,epoch_rec)
 
 print('F1',2*(epoch_prec*epoch_rec)/(epoch_prec+epoch_rec))
+
+
+print('ARI',np.mean(aris))
