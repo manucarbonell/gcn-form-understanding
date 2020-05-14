2daf4b348e1fd123d0e7574a095be884988a2382
manucarbonell <manucarbonell@github.com>
2020-05-14 12:45:52 +0200

    Targets as one hot tensors

--- datasets.py
+++ datasets.py
@@ -62,7 +62,7 @@ class FUNSD(data.Dataset):
 
     def __getitem__(self, index):
         # Read the graph and label
-        G,target,entity_labels,entity_links =self.read_annotations(self.files[index])
+        G,group_labels,entity_labels,entity_links =self.read_annotations(self.files[index])
 
         # Convert to DGL format
         node_label = torch.stack([torch.tensor(v['position']) for k,v in G.nodes.items()]).float()
@@ -70,15 +70,23 @@ class FUNSD(data.Dataset):
 
         node_word = torch.stack([torch.tensor(v['w_embed']) for k,v in G.nodes.items()]).float()
         node_entity = torch.stack([torch.tensor(v['entity']) for k,v in G.nodes.items()]).float()
-        
+        # ENSURE BIDIRECTED
         g_in = dgl.transform.knn_graph(node_label,10)
         g_in = dgl.to_bidirected(g_in)
 
         g_in.ndata['position'] =node_label.float()
         g_in.ndata['w_embed'] =node_word.float()
         g_in.ndata['entity'] = node_entity
-    
-        return g_in, target,entity_labels,entity_links
+   
+
+
+        input_edges = torch.stack(g_in.edges()).t().tolist()            
+        edges = list(map(tuple, input_edges))
+        target_edges = group_labels
+        group_labels = torch.tensor([target_edges[e] for e in edges])
+
+
+        return g_in,group_labels,entity_labels,entity_links
 
     def label2class(self, label):
         # Converts the numeric label to the corresponding string
@@ -120,11 +128,14 @@ class FUNSD(data.Dataset):
                 node_text[word_idx]=self.embeddings[word['text']]
                 node_entity[word_idx]=entity_idx
                 word_idx+=1
-            entity_positions.append(np.array(entity['box'][0:2]))
+            entity_position = np.array([word['box'][:2] for word in entity['words']]).mean(axis=0)
+            entity_positions.append(entity_position)
             entity_labels.append((self.class2label(entity['label'])))
             entity_idx+=1
         entity_positions = torch.tensor(entity_positions)
+        entity_positions = ( (entity_positions.float() - entity_positions.float().mean(0))/ entity_positions.float().std(0))
         entity_labels = torch.tensor(entity_labels)
+        entity_labels=torch.cat([entity_labels.view([-1,1]).float(),entity_positions],dim=1)
         entity_graph = dgl.transform.knn_graph(entity_positions,10)
         entity_graph_edges = torch.t(torch.stack([entity_graph.edges()[0],entity_graph.edges()[1]]))
         entity_link_labels = []
