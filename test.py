c4d246756f1a452f74c0c65c08df377f543fea76
manucarbonell <manucarbonell@github.com>
2020-05-14 17:10:59 +0200

    Test entity linking

--- test.py
+++ test.py
@@ -42,18 +42,30 @@ precisions =[]
 print ("Validation on test set")
 aris = []
 labeling_f1 = []
+linking_f1 = []
 for iter, (input_graph, group_labels,entity_labels,link_labels) in enumerate(test_loader):
+    print('Testing... ',100*float(iter)/len(test_loader),'%',end='\r')
     group_prediction,entity_class,entity_position,entity_link_score = model(input_graph,group_labels)
 
-    group_prediction[group_prediction>model.thres]=1.
-    precision,recall,ari = test_grouping(input_graph,group_prediction,group_labels[0])
+    group_prediction[group_prediction>model.thres]=1
+    group_prediction[group_prediction<model.thres]=0
+    group_prec,group_rec,ari = test_grouping(input_graph,group_prediction,group_labels[0])
+
+    link_prec,link_rec= test_linking(entity_link_score,link_labels,threshold= float(entity_link_score.mean()))
 
     label_prec,label_rec =test_labeling(input_graph,entity_class,entity_position,entity_labels)
-    recalls.append(recall)
-    precisions.append(precision)
+    recalls.append(group_rec)
+    precisions.append(group_prec)
     aris.append(ari)
-
-    labeling_f1.append(2*(label_prec*label_rec)/(label_prec+label_rec))
+    if (label_prec>0 and label_rec>0):
+        labeling_f1.append(2*(label_prec*label_rec)/(label_prec+label_rec))
+    else:
+        labeling_f1.append(0)
+    
+    if (link_prec>0 and link_rec>0):
+        linking_f1.append(2*(link_prec*link_rec)/(link_prec+link_rec))
+    else:
+        linking_f1.append(0)
 
 epoch_prec = np.mean(precisions)
 epoch_rec = np.mean(recalls)
@@ -64,3 +76,4 @@ print('Grouping precision, recall & F1:',epoch_prec,epoch_rec,2*(epoch_prec*epoc
 
 print('ARI',np.mean(aris))
 print('Labeling F1',np.mean(labeling_f1))
+print('Linking F1',np.mean(linking_f1))
