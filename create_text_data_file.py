8cefbda7748ce423246fbbc93b91d0489365c88f
manucarbonell <manucarbonell@github.com>
2023-01-11 12:00:38 +0000

    text_data generation

--- create_text_data_file.py
+++ create_text_data_file.py
@@ -2,6 +2,9 @@ import os
 import json
 import glob
 import sys
+# Create text_data.txt
+# Usage :
+# python3 create_text_data_file.py dataset/training_data/annotations
 
 def create_file():
     text_out_f = open('text_data.txt','w')
@@ -9,4 +12,6 @@ def create_file():
         data = json.load(open(f,'r'))
         for entity in data['form']:
             text_out_f.write(entity['text']+' ')
+if __name__=="__main__":
 
+    create_file()
